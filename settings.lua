dc747f8f385cddaeef1f01f0d641880d53f2c440
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-06-02 22:46:35 +0000

    Add option to disable biters reincarnate from fire death

--- settings.lua
+++ settings.lua
@@ -1,20 +1,29 @@
-data:extend({
-	{
-		name = "turn-to-tree-chance-percent",
-		type = "int-setting",
-		default_value = 100,
-		minomum_value = 0,
-		maximum_value = 100,
-		setting_type = "runtime-global",
-		order = "1001"
-	},
-	{
-		name = "burst-into-flames-chance-percent",
-		type = "int-setting",
-		default_value = 1,
-		minomum_value = 0,
-		maximum_value = 100,
-		setting_type = "runtime-global",
-		order = "1002"
-	}
-})
+data:extend(
+    {
+        {
+            name = "turn-to-tree-chance-percent",
+            type = "int-setting",
+            default_value = 100,
+            minomum_value = 0,
+            maximum_value = 100,
+            setting_type = "runtime-global",
+            order = "1001"
+        },
+        {
+            name = "burst-into-flames-chance-percent",
+            type = "int-setting",
+            default_value = 1,
+            minomum_value = 0,
+            maximum_value = 100,
+            setting_type = "runtime-global",
+            order = "1002"
+        },
+        {
+            name = "prevent-biters-reincarnating-from-fire-death",
+            type = "bool-setting",
+            default_value = false,
+            setting_type = "runtime-global",
+            order = "1003"
+        }
+    }
+)
